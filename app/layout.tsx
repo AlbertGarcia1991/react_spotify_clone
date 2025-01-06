1dc330ccd4566dad1dc64123c48c48518442be6b
Albert Garcia Plaza <gplaza91@gmail.com>
2025-01-06 17:57:31 +0000

    Feat:Supabase DB and Auth Set up Supabase DB and project connection Add sign-up, log-in, and logout functionalities
    https://www.youtube.com/watch?v=2aeMRB8LL4o 2:24:22

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import { Figtree } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import UserProvider from "@/providers/UserProvider";
+import ModalProvider from "@/providers/ModalProvider";
+import ToasterProvider from "@/providers/ToasterProvider";
 
 const font = Figtree({
   subsets: ["latin"],
@@ -19,9 +23,15 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Sidebar>
-          {children}
-        </Sidebar>
+        <SupabaseProvider>
+          <UserProvider>
+            <ToasterProvider />
+            <ModalProvider />
+            <Sidebar>
+              {children}
+            </Sidebar>
+          </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   );
--- components/Header.tsx
+++ components/Header.tsx
@@ -4,8 +4,13 @@ import { useRouter } from "next/navigation";
 import { BiSearch } from "react-icons/bi";
 import { HiHome } from "react-icons/hi";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
+import { FaUserAlt } from "react-icons/fa";
 import { twMerge } from "tailwind-merge";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
+import { useSupabaseClient } from "@supabase/auth-helpers-react";
+import { useUser } from "@/hooks/useUser";
+import toast from "react-hot-toast";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -16,10 +21,22 @@ const Header: React.FC<HeaderProps> = ({
     children,
     className,
 }) => {
+    const authModal = useAuthModal();
     const router = useRouter();
 
-    const handleLogout = () => {
-        // Handle logout
+    const supabaseClient = useSupabaseClient();
+    const { user, subscription } = useUser();
+
+    const handleLogout = async () => {
+        const { error } = await supabaseClient.auth.signOut();
+        // TODO: Reset any playing songs in the future
+        router.refresh();
+
+        if (error) {
+            toast.error(error.message);
+        } else {
+            toast.success("Logged out successfully");
+        }
     };
 
     return (
@@ -53,19 +70,30 @@ const Header: React.FC<HeaderProps> = ({
                     </button>
                 </div>
                 <div className="flex justify-between items-center gap-x-4">
+                    {user ? (
+                        <div className="flex gap-x-4 items-center">
+                            <Button onClick={handleLogout} className="bg-white px-6 py-2">
+                                Logout
+                            </Button>
+                            <Button onClick={() => router.push('/account')} className="bg-white">
+                                <FaUserAlt />
+                            </Button>
+                        </div>
+                    ) : (
                     <>
                     {/* Conent dynamic preparing for the future when we are either logged in or not */}
                         <div>
-                            <Button onClick={() => {}} className="bg-transparent text-neutral-300 font-medium">
+                            <Button onClick={authModal.onOpen} className="bg-transparent text-neutral-300 font-medium">
                                 Sign Up
                             </Button>
                         </div>
                         <div>
-                            <Button onClick={() => {}} className="bg-white px-6 py-2">
+                            <Button onClick={authModal.onOpen} className="bg-white px-6 py-2">
                                 Log in
                             </Button>
                         </div>
                     </>
+                    )}
                 </div>
             </div>
             {children}
